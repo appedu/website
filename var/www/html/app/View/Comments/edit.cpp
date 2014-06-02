f19d31347a0ba6375a783d97cc5368781515c151
appedu <timothy.yu@appedu.co>
2014-06-02 17:49:37 +0800

    Create edit.cpp

---
+++ var/www/html/app/View/Comments/edit.cpp
@@ -0,0 +1,55 @@
+
+<div id="page-container" class="row">
+
+	<div class='.col-md-12'>
+		<div class="btn-group pull-right">
+				<?php echo $this->Form->postLink(__(''), array('action' => 'delete', $this->Form->value('Comment.id')), array('class' => 'icon-trash btn btn-danger'), __('Are you sure you want to delete # %s?', $this->Form->value('Comment.id'))); ?>
+				<?php echo $this->Html->link(__(''), array('action' => 'index'), array('class' => 'btn icon-undo')); ?>
+		</div><!-- /.btn-group pull-right -->
+		<h2><?php echo __('Edit Comment'); ?></h2>
+	</div>
+
+	<div class="comments form">
+	
+		<?php echo $this->Form->create('Comment', array('role' => 'form', 
+			'inputDefaults' => array(
+				'div' => array(
+					'class' => 'form-group'
+				), 'label' => array(
+					'class' => 'col-sm-2 control-label no-padding-right'
+				),
+				'between' => '<div class=\'col-xs-5\'>',
+				'after' => '</div>',
+				'format' => array('before', 'label', 'between', 'input', 'after', 'error')
+			), 'class' => 'form-horizontal')); ?>
+
+			<fieldset>
+
+					<?php echo $this->Form->input('id', array('class' => 'form-control')); ?>
+					
+					<?php echo $this->Form->input('description', array('class' => 'form-control')); ?>
+					
+					<?php echo $this->Form->input('isAnonymous', array(
+						'class' => 'ace ace-switch',
+						'between' => '<div class=\'col-xs-5\'><label>',
+						'after' => '<span class=\'lbl\'></span></label></div>',
+					)); ?>
+					
+					<?php echo $this->Form->input('user_id', array('class' => 'form-control')); ?>
+					
+					<?php echo $this->Form->input('answer_id', array('class' => 'form-control')); ?>
+					
+
+					<div class='clear-fix form-action'>
+						<div class='col-md-offset-6 col-md-7'>
+							<?php echo $this->Form->submit('Submit', array('class' => 'btn btn-large btn-primary')); ?>
+						</div>
+					</div>
+
+			</fieldset>
+
+			<?php echo $this->Form->end(); ?>
+
+	</div><!-- /.form -->
+		
+</div><!-- /#page-container .row-fluid -->
