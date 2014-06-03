ac0bae0d9d53183d765f667d9c7a6b24df4c0547
appedu <timothy.yu@appedu.co>
2014-06-03 11:50:31 +0800

    Create User.php

---
+++ var/www/html/app/Model/User.php
@@ -0,0 +1,320 @@
+<?php
+
+App::uses('AppModel', 'Model');
+
+/**
+ * User Model
+ *
+ * @property School $School
+ * @property Answer $Answer
+ * @property Attachment $Attachment
+ * @property Comment $Comment
+ * @property Notification $Notification
+ * @property Question $Question
+ * @property Rating $Rating
+ * @property Student $Student
+ * @property Subject $Subject
+ * @property Group $Group
+ * @property Question $Question
+ */
+class User extends AppModel {
+
+    /**
+     * Display field
+     *
+     * @var string
+     */
+    public $displayField = 'username';
+    //public $useTable = 'students_subjects';
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+//	public $validate = array(
+//		'username' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'email' => array(
+//			'email' => array(
+//				'rule' => array('email'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'role' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'dob' => array(
+//			'date' => array(
+//				'rule' => array('date'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'gender' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'form' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'class' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'phone_number' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'first_name' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//		'last_name' => array(
+//			'notEmpty' => array(
+//				'rule' => array('notEmpty'),
+//				//'message' => 'Your custom message here',
+//				//'allowEmpty' => false,
+//				//'required' => false,
+//				//'last' => false, // Stop validation after this rule
+//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+//			),
+//		),
+//	);
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
+
+    /**
+     * belongsTo associations
+     *
+     * @var array
+     */
+    public $belongsTo = array(
+        'School' => array(
+            'className' => 'School',
+            'foreignKey' => 'school_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
+
+    /**
+     * hasMany associations
+     *
+     * @var array
+     */
+    public $hasMany = array(
+        'Answer' => array(
+            'className' => 'Answer',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+        'Attachment' => array(
+            'className' => 'Attachment',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+        'Comment' => array(
+            'className' => 'Comment',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+        'Notification' => array(
+            'className' => 'Notification',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+        'Question' => array(
+            'className' => 'Question',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+        'Rating' => array(
+            'className' => 'Rating',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+//        'Student' => array(
+//            'className' => 'Student',
+//            'foreignKey' => 'user_id',
+//            'dependent' => false,
+//            'conditions' => '',
+//            'fields' => '',
+//            'order' => '',
+//            'limit' => '',
+//            'offset' => '',
+//            'exclusive' => '',
+//            'finderQuery' => '',
+//            'counterQuery' => ''
+//        ),
+//        'Subject' => array(
+//            'className' => 'Subject',
+//            'foreignKey' => 'subject_id',
+//        ),
+        'User' => array(
+            'className' => 'StudentsSubject',
+            'foreignKey' => 'user_id',
+        ),
+        'Visession' => array(
+            'className' => 'Visession',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+        'StudentsSubject' => array(
+            'className' => 'StudentsSubject',
+            'foreignKey' => 'user_id',
+            'dependent' => false,
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'exclusive' => '',
+            'finderQuery' => '',
+            'counterQuery' => ''
+        ),
+    );
+
+    /**
+     * hasAndBelongsToMany associations
+     *
+     * @var array
+     */
+    public $hasAndBelongsToMany = array(
+        'Group' => array(
+            'className' => 'Group',
+            'joinTable' => 'groups_users',
+            'foreignKey' => 'user_id',
+            'associationForeignKey' => 'group_id',
+            'unique' => 'keepExisting',
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'finderQuery' => '',
+        ),
+        'Question' => array(
+            'className' => 'Question',
+            'joinTable' => 'questions_users',
+            'foreignKey' => 'user_id',
+            'associationForeignKey' => 'question_id',
+            'unique' => 'keepExisting',
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'limit' => '',
+            'offset' => '',
+            'finderQuery' => '',
+        )
+    );
+
+}
